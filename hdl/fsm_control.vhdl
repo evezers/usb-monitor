47291ba54883c32ce2daf7f09ffaaed8b51b417e
Evgeny Ezers <30502462+evezers@users.noreply.github.com>
2023-12-25 11:07:21 +0300

    Connect fsm_ulpi_transmit into main fsm

--- hdl/fsm_control.vhdl
+++ hdl/fsm_control.vhdl
@@ -12,7 +12,16 @@ entity fsm_control is
         o_register_request          : out   std_logic;
         o_register_address          : out   std_logic_vector(7 downto 0);
         o_register_data             : out   std_logic_vector(7 downto 0);
-        i_register_busy : in    std_logic;
+        i_register_busy             : in    std_logic;
+
+        -- fsm_ulpi_transmit
+        o_fsm_ulpi_transmit_enable : out   std_logic;
+        o_transmit_end             : out   std_logic;
+        o_transmit_request         : out   std_logic;
+        o_transmit_pid             : out   std_logic_vector(3 downto 0);
+        o_transmit_data            : out   std_logic_vector(7 downto 0);
+        i_transmit_busy            : in    std_logic;
+        i_transmit_hold            : in    std_logic;
 
         ulpi_config_finished : in    std_logic
     );
@@ -20,10 +29,18 @@ end entity fsm_control;
 
 architecture rtl of fsm_control is
 
+    -- common
+    signal r_register_request : std_logic;
+    signal r_register_data    : std_logic_vector(7 downto 0);
+
+    -- fsm_ulpi_registers
     signal r_fsm_ulpi_registers_enable : std_logic;
-    signal r_register_request          : std_logic;
     signal r_register_address          : std_logic_vector(7 downto 0);
-    signal r_register_data             : std_logic_vector(7 downto 0);
+
+    -- fsm_ulpi_transmit
+    signal r_fsm_ulpi_transmit_enable : std_logic;
+    signal r_transmit_end             : std_logic;
+    signal r_transmit_pid             : std_logic_vector(3 downto 0);
 
     type fsm_control_state_type is (
         ulpi_config_state,
@@ -41,6 +58,8 @@ begin
     o_register_request          <= r_register_request;
     o_register_address          <= r_register_address;
     o_register_data             <= r_register_data;
+    o_transmit_pid              <= r_transmit_pid;
+    o_transmit_end              <= r_transmit_end;
 
     process (enable, clk, reset) is
     begin
@@ -50,11 +69,12 @@ begin
         elsif (reset = '1') then
             state <= ulpi_config_state;
 
-            -- data_head_r <= (others => '0');
             r_register_data             <= (others => '0');
             r_register_address          <= (others => '0');
+            r_transmit_pid              <= (others => '0');
             r_register_request          <= '0';
             r_fsm_ulpi_registers_enable <= '0';
+            r_transmit_end              <= '0';
         elsif (rising_edge(clk)) then
 
             case state is
@@ -112,39 +132,41 @@ begin
 
             when ulpi_config_state =>
 
-                o_register_address          <= b"1000_1010";
-                o_register_data             <= b"0000_0000";
-                o_register_request          <= '1';
-                o_fsm_ulpi_registers_enable <= '1';
+                r_register_address          <= b"1000_1010";
+                r_register_data             <= b"0000_0000";
+                r_register_request          <= '1';
+                r_fsm_ulpi_registers_enable <= '1';
 
             when usb_connect_state =>
 
-                o_register_request          <= '0';
-                o_fsm_ulpi_registers_enable <= '0';
+                r_register_request          <= '0';
+                r_fsm_ulpi_registers_enable <= '0';
+
+                r_fsm_ulpi_transmit_enable <= '1';
 
             when usb_config_state =>
 
-                if (ulpi_config_finished = '1') then
-                    state <= usb_idle_state;
-                else
-                    state <= state;
-                end if;
+            -- if (ulpi_config_finished = '1') then
+            --     state <= usb_idle_state;
+            -- else
+            --     state <= state;
+            -- end if;
 
             when usb_idle_state =>
 
-                if (ulpi_config_finished = '1') then
-                    state <= usb_disconnect;
-                else
-                    state <= state;
-                end if;
+            -- if (ulpi_config_finished = '1') then
+            --     state <= usb_disconnect;
+            -- else
+            --     state <= state;
+            -- end if;
 
             when usb_disconnect =>
 
-                if (ulpi_config_finished = '1') then
-                    state <= usb_connect_state;
-                else
-                    state <= state;
-                end if;
+        -- if (ulpi_config_finished = '1') then
+        --     state <= usb_connect_state;
+        -- else
+        --     state <= state;
+        -- end if;
 
         end case;
 
